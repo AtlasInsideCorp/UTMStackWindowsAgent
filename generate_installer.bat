b44ff96dc9b0af048869f11b88cb9cc50553e216
adbenitez <asieldbenitez@gmail.com>
2021-02-11 00:44:53 -0500

    update generate_installer.bat, update pip and dependencies before generating installer

--- generate_installer.bat
+++ generate_installer.bat
@@ -1,2 +1,17 @@
-%CD%\compiler\ISCC.exe installer.iss
-more
+@echo [*] Updating project...
+@git pull
+@echo [*] Done.
+
+@echo [*] Updating dependencies...
+@%CD%\assets\Python37\python.exe -m pip install -U pip
+@%CD%\assets\Python37\python.exe -m pip install -U -r requirements.txt
+@if NOT ["%errorlevel%"]==["0"] (
+    pause
+    exit /b %errorlevel%
+)
+@echo [*] Done.
+
+@echo [*] Generating Installer...
+@%CD%\compiler\ISCC.exe installer.iss
+@echo [*] Done.
+@pause
